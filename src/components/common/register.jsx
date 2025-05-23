c39eefb679a8dbb73f44938a9449b0308401dcb4
Ivon-1 <IvanAlvz12@outlook.es>
2025-05-23 19:50:31 +0200

    Una vez registrado ir al home

--- src/components/common/register.jsx
+++ src/components/common/register.jsx
@@ -63,6 +63,7 @@ export function Registro() {
 
         // limpiamos error
         setError("");
+        navigate("/");
 
 
         // llamamos funcion con parametros. password_confirmation para verificar campo
