5ffa468cb67d200fb7d85741c211c9a6f0dfa1a7
Ivon-1 <IvanAlvz12@outlook.es>
2025-06-10 00:44:42 +0200

    Cambio endpoint

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 const env = {
     //url_local: "http://127.0.0.1:8000/",
-    url_produccion: "https://tfg-final-1-main-awmj2t.laravel.cloud/",
+    url_produccion: "https://tfg-final-main-5fj4zp.laravel.cloud/",
     token: "",
     nombre_app: "Modtech",
     version: "1"
