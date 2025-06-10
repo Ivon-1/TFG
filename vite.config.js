2852277f3d1129ac3ad33d5f56c381d252c3665b
Ivon-1 <IvanAlvz12@outlook.es>
2025-06-10 14:03:44 +0200

    Actualizacion versiones

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+        additionalData: `@use "sass:math";`
+      }
+    }
+  }
 })
